d0b3f6d4b66dbba3b92e0dd9780efb63b9c0df1b
vutran <vutran1412@icloud.com>
2019-06-10 12:42:02 -0700

    initial commit

--- Egg Timer/ViewController.swift
+++ Egg Timer/ViewController.swift
@@ -9,10 +9,60 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var timer = Timer()
+    
+    @IBOutlet weak var timerLabel: UILabel!
+    
+    @IBAction func pauseButton(_ sender: UIBarButtonItem) {
+        timer.invalidate()
+    }
+    
+    @IBAction func addTimeButton(_ sender: UIBarButtonItem) {
+        let time = 10
+        if let currentTime = Int(timerLabel.text!) {
+            timerLabel.text = "\(currentTime + time)"
+        }
+        
+    }
+    @IBAction func subtractTimeButton(_ sender: UIBarButtonItem) {
+        let time = 10
+        if let currentTime = Int(timerLabel.text!) {
+            if currentTime >= 10 {
+                timerLabel.text = "\(currentTime - time)"
+            }
+        }
+    }
+    
+    @IBAction func resetButton(_ sender: UIBarButtonItem) {
+        let startTime = 210
+        timerLabel.text = "\(startTime)"
+    }
+    
+    @IBAction func playButton(_ sender: UIBarButtonItem) {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(processTimer), userInfo: nil, repeats: true)
+    }
+    
+    @objc func processTimer() {
+        if var time = Int(timerLabel.text!) {
+            time -= 1
+            timerLabel.text = "\(time)"
+            if time == 0 {
+                timer.invalidate()
+            }
+        }
+        
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
 
 
